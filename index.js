327ab61d5030ddf9371f9e50d653cafbf4ab72b6
kenzotrindade <kenzo.trindade@gmail.com>
2026-03-28 14:22:37 +0100

    Starting page finished

---
+++ index.js
@@ -0,0 +1,12 @@
+VANTA.NET({
+  el: "body",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x333333,
+  backgroundColor: 0x0,
+});
