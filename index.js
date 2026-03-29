fd4c63d6493e7e3cb971430fbde9f4b5d7959fde
kenzotrindade <kenzo.trindade@gmail.com>
2026-03-29 12:25:52 +0200

    Add: Skills section in front

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-VANTA.NET({
-  el: "body",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.0,
-  minWidth: 200.0,
-  scale: 1.0,
-  scaleMobile: 1.0,
-  color: 0x333333,
-  backgroundColor: 0x0,
+document.addEventListener("DOMContentLoaded", () => {
+  VANTA.NET({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color: 0x333333,
+    backgroundColor: 0x0,
+  });
 });
